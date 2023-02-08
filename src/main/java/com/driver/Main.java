bd11be61ee28adca21e88da3edb1b88bf1e8491d
mahesh-7101 <86470981+mahesh-7101@users.noreply.github.com>
2023-02-08 14:31:56 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        public int product(int x,int y){
+        return x*y;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+    }
+    public static void main(String [] args){
+        Product p=new Product();
+        p.product(5,2);
+        p.product(5,2,3);
+        p.product(4,8);
+    }
 }
